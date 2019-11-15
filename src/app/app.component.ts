8a9cfeb96759ac1c186897dde7a7a7fcfa1014aa
ayan.das <ayan.das@experionglobal.com>
2019-11-15 16:49:59 +0530

    made an app that finds git user profile on prompt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { error } from 'util';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-assignment';
+  username: ' ';
+  data: any;
+  datalocal: any;
+  showspinner = false;
+  constructor(private http: HttpClient, private _snackBar: MatSnackBar) { }
+
+  search() {
+    if (this.username) {
+      this.showspinner = true;
+      this.datalocal = localStorage.getItem(this.username);
+      if (!this.datalocal) {
+        this.showspinner = true;
+        console.log(this.username);
+        this.http.get('https://api.github.com/users/' + this.username).subscribe(Response => {
+          this.data = Response;
+          console.log(this.data);
+          this.showspinner = false;
+          localStorage.setItem(this.username, JSON.stringify(this.data));
+        }, err => {
+          this.showspinner = false;
+          this.data = '';
+          this.openSnackBar('bad user credentials', 'ok');
+        });
+      } else {
+        this.data = JSON.parse(this.datalocal);
+        this.showspinner = false;
+        console.log(this.data);
+      }
+    } else {
+      this.data = '';
+      this.openSnackBar('provide username', 'ok');
+    }
+  }
+  openSnackBar(message, action) {
+    this._snackBar.open(message, action, {
+      duration: 1000,
+      verticalPosition: 'top',
+      horizontalPosition: 'right',
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule,
+    HttpClientModule,
+    FormsModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
+
